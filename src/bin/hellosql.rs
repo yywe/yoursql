8cede3580dff5b2f8f65babe98f2003c08c3a212
Hulua <stweiyy@gmail.com>
2023-04-28 22:45:58 -0400

    filter log add unit test

--- src/bin/hellosql.rs
+++ src/bin/hellosql.rs
@@ -1,16 +1,20 @@
 use yoursql::storage::{SledStore, Catalog};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
 use tracing::{debug, info, span, Level};
-use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
 use anyhow::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::registry()
-        .with(fmt::layer())
-        .init();
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"))
+        .add_directive("sled=off".parse().unwrap());
+    let subscriber = FmtSubscriber::builder()
+        .with_env_filter(env_filter)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
     info!("starting the database...");
 
-    let ss = SledStore::init("/home/yy/Learning/Database/yoursql/tempdb").await?;
-
+    let ss = SledStore::init("./tempdb").await?;
 
     let ans = ss.listdbs()?;
     println!("list of dbs:{:#?}",ans);
--- src/storage/sled.rs
+++ src/storage/sled.rs
@@ -1,14 +1,13 @@
 use super::Catalog;
-use anyhow::Context;
+use crate::error::StorageError;
 use anyhow::Result;
+use async_fs as fs;
 use async_trait::async_trait;
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 use sled::Db;
-use async_fs as fs;
 use std::path::Path;
 use tracing::info;
-use crate::error::StorageError;
 
 pub struct SledStore {
     pub root: Box<Path>,
@@ -57,19 +56,23 @@ impl SledStore {
         if self.curdbid != 0 {
             return Err(StorageError::MustInMaster(self.curdbid).into());
         }
-        let dblist = self.db.scan_prefix("db").map(|record|{
-            let (_, encoded) = record?;
-            let decoded: DbMeta = bincode::deserialize(&encoded)?;
-            Ok(decoded)
-        }).collect::<Result<Vec<_>>>()?;
+        let dblist = self
+            .db
+            .scan_prefix("db")
+            .map(|record| {
+                let (_, encoded) = record?;
+                let decoded: DbMeta = bincode::deserialize(&encoded)?;
+                Ok(decoded)
+            })
+            .collect::<Result<Vec<_>>>()?;
         Ok(dblist)
     }
-
 }
 
 #[async_trait]
 impl Catalog for SledStore {
     async fn create_database(&self, database_name: &String) -> Result<()> {
+        info!("creating database {}", database_name);
         if self.curdbid != 0 {
             return Err(StorageError::MustInMaster(self.curdbid).into());
         }
@@ -77,16 +80,21 @@ impl Catalog for SledStore {
         if dbpath.exists() {
             return Err(StorageError::DBRegistered(database_name.clone()).into());
         }
-        if self.listdbs()?.into_iter().find(|meta| meta.name==*database_name).is_some(){
+        if self
+            .listdbs()?
+            .into_iter()
+            .find(|meta| meta.name == *database_name)
+            .is_some()
+        {
             return Err(StorageError::DBRegistered(database_name.clone()).into());
         }
         // todo add lock when update next_dbid, racing condition
         let next_dbid = match self.db.get("next_dbid")? {
             Some(v) => {
-                let bytes: [u8;4] = v.as_ref().try_into()?;
+                let bytes: [u8; 4] = v.as_ref().try_into()?;
                 u32::from_be_bytes(bytes)
             }
-            None => return Err(StorageError::NextDBIDNotFound.into())
+            None => return Err(StorageError::NextDBIDNotFound.into()),
         };
         sled::open(self.root.join(database_name))?;
         let dbmeta = DbMeta {
@@ -97,15 +105,20 @@ impl Catalog for SledStore {
         let encoded: Vec<u8> = bincode::serialize(&dbmeta)?;
         // todo if error should delete the folder
         self.db.insert(format!("db{}", next_dbid), encoded)?;
-        self.db.insert("next_dbid", &(next_dbid+1).to_be_bytes())?;
+        self.db
+            .insert("next_dbid", &(next_dbid + 1).to_be_bytes())?;
         Ok(())
     }
 
     async fn drop_database(&self, database_name: &String) -> Result<()> {
+        info!("droping database {}", database_name);
         if self.curdbid != 0 {
             return Err(StorageError::MustInMaster(self.curdbid).into());
         }
-        let dblist = self.listdbs()?.into_iter().find(|meta| meta.name==*database_name);
+        let dblist = self
+            .listdbs()?
+            .into_iter()
+            .find(|meta| meta.name == *database_name);
         match dblist {
             Some(meta) => {
                 self.db.remove(format!("db{}", meta.id))?;
@@ -121,8 +134,29 @@ impl Catalog for SledStore {
 #[cfg(test)]
 mod test {
     use super::*;
-    #[test]
-    fn test_sledstore() {
-        _ = SledStore::init("/home/yy/Learning/Database/yoursql/tdb");
+    use tracing_subscriber::{EnvFilter, FmtSubscriber};
+    #[tokio::test]
+    async fn test_sledstore() -> Result<()> {
+        let env_filter = EnvFilter::try_from_default_env()
+            .unwrap_or_else(|_| EnvFilter::new("info"))
+            .add_directive("sled=off".parse().unwrap());
+        let subscriber = FmtSubscriber::builder()
+            .with_env_filter(env_filter)
+            .finish();
+        tracing::subscriber::set_global_default(subscriber).unwrap();
+        let test_folder = "./tdb";
+        let ss = SledStore::init(test_folder).await?;
+        let initdbs = ss.listdbs()?;
+        assert_eq!(initdbs.len(), 1);
+        assert_eq!(initdbs[0].name, "master");
+        ss.create_database(&"newdb".into()).await?;
+        let alldbs = ss.listdbs()?;
+        assert_eq!(alldbs.len(), 2);
+        assert_eq!(alldbs[1].name, "newdb");
+        ss.drop_database(&"newdb".into()).await?;
+        let afterdrop = ss.listdbs()?;
+        assert_eq!(afterdrop.len(), 1);
+        fs::remove_dir_all(test_folder).await?;
+        Ok(())
     }
 }
