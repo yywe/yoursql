f5828f2811d1005aef61875f8d992845dccc443d
Hulua <stweiyy@gmail.com>
2023-07-01 23:19:54 -0400

    update session

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -1,5 +1,8 @@
+use crate::catalog::Catalog;
 use crate::catalog::CatalogList;
+use crate::catalog::MemoryCatalog;
 use crate::catalog::MemoryCatalogList;
+use crate::catalog::MemorySchema;
 use crate::common::config::ConfigOptions;
 use chrono::{DateTime, Utc};
 use std::sync::Arc;
@@ -33,7 +36,22 @@ impl SessionState {
 
 impl SessionContext {
     pub fn new_inmemory_ctx() -> Self {
-        let state = SessionState::new(ConfigOptions::new(), Arc::new(MemoryCatalogList::new()));
+        let config = ConfigOptions::new();
+        let catalogs = MemoryCatalogList::new();
+        if config.catalog.create_default_catalog_and_schema {
+            let default_catalog = MemoryCatalog::new();
+            default_catalog
+                .register_schema(
+                    &config.catalog.default_schema,
+                    Arc::new(MemorySchema::new()),
+                )
+                .expect("failed to register schema");
+            catalogs.register_catalog(
+                config.catalog.default_catalog.clone(),
+                Arc::new(default_catalog),
+            );
+        }
+        let state = SessionState::new(config, Arc::new(catalogs));
         Self {
             session_id: state.session_id.clone(),
             session_start_time: Utc::now(),
