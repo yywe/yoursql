3f8a6d7a0818981847e3a44a86075a57fe6fae2b
weiy49 <weiy49@mcmaster.ca>
2024-05-31 10:41:31 -0400

    update build.rs

--- build.rs
+++ build.rs
@@ -1,10 +1,20 @@
 use std::env;
+
 fn main() {
     // PLEASE CHANGE THIS TO YOUR OWN LLVM VERSION AND PATH
     // For linux used: clang+llvm-16.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
     //env::set_var("LLVM_SYS_160_PREFIX", "~/Tools/clangllvm");
     // For macos with intel used: clang+llvm-15.0.7-x86_64-apple-darwin21.0.tar.xz
-    env::set_var("LLVM_SYS_150_PREFIX", "~/Tools/clangllvm");
+
+
+    // set accourdingly
+    #[cfg(target_os="macos")]
+    let llvm_path = "/Users/yongyongwei/Tools/clangllvm";
+
+    #[cfg(target_os="linux")]
+    let llvm_path = "/home/yy/Tools/clangllvm";
+
+    env::set_var("LLVM_SYS_150_PREFIX", llvm_path);
 }
 /*
 required by rust-analyzer
