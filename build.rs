77c62dcccc2ee24267d8deab78026e75d7ccb63d
weiy49 <stweiyy@gmail.com>
2024-05-31 19:19:08 -0400

    update build.rs

--- build.rs
+++ build.rs
@@ -1,27 +1,34 @@
+extern crate which;
 use std::env;
-
+use which::which;
 fn main() {
     // PLEASE CHANGE THIS TO YOUR OWN LLVM VERSION AND PATH
-    // For linux used: clang+llvm-16.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-    //env::set_var("LLVM_SYS_160_PREFIX", "~/Tools/clangllvm");
-    // For macos with intel used: clang+llvm-15.0.7-x86_64-apple-darwin21.0.tar.xz
-
-
-    // set accourdingly
-    #[cfg(target_os="macos")]
-    let llvm_path = "/Users/yongyongwei/Tools/clangllvm";
-
-    #[cfg(target_os="linux")]
-    let llvm_path = "/home/yy/Tools/clangllvm";
-
-    env::set_var("LLVM_SYS_150_PREFIX", llvm_path);
+    // NOTE: For macos with intel used: clang+llvm-15.0.7-x86_64-apple-darwin21.0.tar.xz
+    // HERE WE USE LLVM_SYS_150_PREFIX for compatiblity with linux
+    let llvm_path = match which("llc") {
+        Ok(path) => {
+            if let Some(parent) = path.parent() {
+                parent.to_str().unwrap().to_string()
+            } else {
+                eprintln!("Error finding 'llc': {:?}", path);
+                std::process::exit(1);
+            }
+        },
+        Err(e) => {
+            eprintln!("Error finding 'llc': {:?}", e);
+            std::process::exit(1);
+        }
+    };
+    unsafe {
+        env::set_var("LLVM_SYS_150_PREFIX", llvm_path);
+    }
 }
 /*
-required by rust-analyzer
+some thing like below is required by rust-analyzer
 .vscode/settings.json
 {
    "rust-analyzer.cargo.extraEnv": {
-        "LLVM_SYS_160_PREFIX": "~/Tools/clangllvm"  
+        "LLVM_SYS_150_PREFIX": "~/Tools/clangllvm"  
     },
 }
 */
